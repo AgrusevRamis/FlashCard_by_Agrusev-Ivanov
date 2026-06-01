9dc9626cb8d12b3aba9aba45fd303bb289cee28c
Никита <ni432109@gmail.com>
2026-06-01 10:01:03 +0300

    Косметический ремонт

--- FlashcardApp.h
+++ FlashcardApp.h
@@ -3,29 +3,6 @@
 #include "Card.h"
 
 class FlashcardApp {
-public:
-    FlashcardApp();
-    bool initAndRun();
-
-private:
-    void processEvents();
-    void render();
-
-    void drawMenuScreen(sf::Vector2i mouse);
-    void drawAddScreen(sf::Vector2i mouse);
-    void drawTestScreen(sf::Vector2i mouse);
-    void drawResultScreen(sf::Vector2i mouse);
-
-    sf::Text txt(const char* s, unsigned sz, sf::Color c, float x, float y);
-    void drawCentered(const char* s, unsigned sz, sf::Color c, float cy);
-    void drawRect(float x, float y, float w, float h, sf::Color fill, sf::Color outline = sf::Color::Transparent);
-    void drawBtn(const char* s, float x, float y, float w, float h, sf::Color fill, sf::Vector2i mouse);
-    bool clicked(sf::Event& e, float x, float y, float w, float h);
-
-    void addUtf8(char* buf, uint32_t code);
-    void shuffle(Card* a, int n);
-
-private:
     sf::RenderWindow win;
     sf::Font font;
     Screen screen;
@@ -55,4 +32,24 @@ private:
     sf::Color GRAY;
     sf::Color TXT;
     sf::Color MUTED;
+    void processEvents();
+    void render();
+
+    void drawMenuScreen(sf::Vector2i mouse);
+    void drawAddScreen(sf::Vector2i mouse);
+    void drawTestScreen(sf::Vector2i mouse);
+    void drawResultScreen(sf::Vector2i mouse);
+
+    sf::Text txt(const char* s, unsigned sz, sf::Color c, float x, float y);
+    void drawCentered(const char* s, unsigned sz, sf::Color c, float cy);
+    void drawRect(float x, float y, float w, float h, sf::Color fill, sf::Color outline = sf::Color::Transparent);
+    void drawBtn(const char* s, float x, float y, float w, float h, sf::Color fill, sf::Vector2i mouse);
+    bool clicked(sf::Event& e, float x, float y, float w, float h);
+
+    void addUtf8(char* buf, uint32_t code);
+    void shuffle(Card* a, int n);
+
+public:
+    FlashcardApp();
+    bool initAndRun();
 };
