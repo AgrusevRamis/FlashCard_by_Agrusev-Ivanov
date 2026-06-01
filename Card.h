6b25d68ce9e434910206d96b9f164e681df07134
Никита <ni432109@gmail.com>
2026-06-01 18:49:31 +0300

    изменен card.h расширено количество карточек

--- Card.h
+++ Card.h
@@ -1,13 +1,13 @@
 #pragma once
 
-const int W = 800;
-const int H = 600;
-const int MAX = 200;
-const int MWORD = 100;
+static const int MAX   = 2048;
+static const int MWORD = 128;
+static const int W     = 800;
+static const int H     = 560;
 
 struct Card {
     char w[MWORD];
     char t[MWORD];
 };
 
-enum Screen { MENU, ADD, TEST, RESULT };
+enum Screen { MENU, FILE_LOAD, ADD, EDIT_LIST, TEST, RESULT };
